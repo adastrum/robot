8c06b83162deb30519754dc2bfce78f4b86d0596
Dmitry Trofimov <trofimovde@outlook.com>
2018-11-18 11:33:30 +0300

    Add domain tests

---
+++ RobotCleaner.Domain.Tests/RobotTests.cs
@@ -0,0 +1,17 @@
+using Xunit;
+
+namespace RobotCleaner.Domain.Tests
+{
+    public class RobotTests
+    {
+        [Fact]
+        public void CreateRobot()
+        {
+            var robot = new Robot();
+        }
+    }
+
+    public class Robot
+    {
+    }
+}
